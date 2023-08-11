65ad053767c9d1d22984745c559b8d52bcdcd071
Clint Mark Gono <clint@clintgono.com>
2023-08-11 19:14:58 +0800

    Rearrange stuff, update packages, introduce services

---
+++ CGono.NagBot.TG.Services/NagBot.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+
+namespace CGono.NagBot.TG.Services
+{
+    public class NagBot : TelegramBotClient
+    {
+        public NagBot(TelegramBotClientOptions options, HttpClient? httpClient = null) : base(options, httpClient)
+        {
+        }
+
+        public NagBot(string token, HttpClient? httpClient = null) : base(token, httpClient)
+        {
+        }
+    }
+}
